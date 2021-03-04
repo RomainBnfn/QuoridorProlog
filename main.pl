12fc0cbd52c655bd66b2683d7feb4e0c761b9c35
RomainBnfn <rbonnefon@ensc.fr>
2021-03-04 18:39:32 +0100

    J'ai fais une fonction qui affiche les pions selon leurs coordonnées

--- main.pl
+++ main.pl
@@ -15,15 +15,43 @@ start :-
 
 
 dessinerTerrain([ [_, _, _], [_, _, _], _, TailleTerrain ]):-
+    write('    1   2   3   4   5   6   7   8   9'), nl,
     I = 1,
     dessinerLigne(I, TailleTerrain).
 
-dessinerLigne(I, T) :-
-    I =< T,
-    Inew is I + 1, 
-    write(I),
+dessinerLigne(I, T) :- % I le numéro de la ligne dessinée.
+    I =< T, % On est dans le terrain.
+    
+    %Dessiner les murs
+    J = 1, % indice
+    write('  .   .   .   .   .   .   .   .   .   .'), nl,
+    write(I), write('  '), dessinerPions(I, J, T, [1, 5], [2, 3]),
+
+    %Dessiner les pions
+
+    Inew is I + 1, % I++
     dessinerLigne(Inew, T).
 
-dessinerLigne(T, T) :-
-    write('.').
+dessinerLigne(T, T) :- %Après la dernière ligne
+    write('  .   .   .   .   .   .   .   .   .   .'), nl.
 
+% I: la position en x
+% J: position en y
+% T: Taille du terrain 
+dessinerPions(I, J, T, [I, J], [Xb, Yb]) :- J =< T,
+    write(' A  '),
+    Jnew is J + 1,
+    dessinerPions(I, Jnew, T, [I,J], [Xb, Yb]).
+
+dessinerPions(I, J, T, [Xa, Ya], [I, J]) :- J =< T,
+    write(' B  '),
+    Jnew is J + 1,
+    dessinerPions(I, Jnew, T, [Xa, Ya], [I, J]).
+
+dessinerPions(I, J, T, [Xa, Ya], [Xb, Yb]) :- J =< T,
+    write('    '),
+    Jnew is J + 1,
+    dessinerPions(I, Jnew, T, [Xa, Ya], [Xb, Yb]).
+
+dessinerPions(_, T, T, _, _) :- nl.
+    
